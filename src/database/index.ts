468b9313e83c35d3c6aa0b17022d865cbe75e553
Augusto Souza <augustojsouza1@gmail.com>
2021-03-23 19:34:15 -0300

    Changed files to LF

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,3 @@
 import { createConnection } from "typeorm";
 
-createConnection();
+createConnection().then(() => console.log("Conexão com o banco sucesso!"));
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
+
 import { router } from './routes';
 import swaggerFile from './swagger.json';
 import './database';
@@ -12,4 +13,4 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
-app.listen(3333, () => console.log("Server is running!"));
+app.listen(3333, () => console.log('Server is running!'));
