9fba366bb787871f24eb1ad3b8bf0d7efd760295
Augusto Souza <augustojsouza1@gmail.com>
2021-03-31 21:05:40 -0300

    Created admin middleware

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -2,13 +2,13 @@ import { Router } from 'express';
 
 import { CreateSpecificationController } from '@modules/cars/useCases/createSpecification/CreateSpecificationController';
 
-import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+import { ensureAdmin } from "@shared/infra/http/middlewares/ensureAdmin";
+import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 
 const specificationRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
-specificationRoutes.use(ensureAuthenticated);
-specificationRoutes.post('/', createSpecificationController.handle);
+specificationRoutes.post('/', ensureAuthenticated, ensureAdmin, createSpecificationController.handle);
 
 export { specificationRoutes };
