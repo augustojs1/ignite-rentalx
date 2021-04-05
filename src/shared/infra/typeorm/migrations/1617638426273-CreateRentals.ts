e2f92da0885dc3e4b339d973103023d832888f55
Augusto Souza <augustojsouza1@gmail.com>
2021-04-05 20:10:32 -0300

    Finished rental functionaliy

--- src/shared/infra/typeorm/migrations/1617638426273-CreateRentals.ts
+++ src/shared/infra/typeorm/migrations/1617638426273-CreateRentals.ts
@@ -24,6 +24,7 @@ export class CreateRentals1617638426273 implements MigrationInterface {
                     {
                         name: "end_date",
                         type: "timestamp",
+                        isNullable: true,
                     },
                     {
                         name: "expected_return_date",
@@ -32,6 +33,7 @@ export class CreateRentals1617638426273 implements MigrationInterface {
                     {
                         name: "total",
                         type: "numeric",
+                        isNullable: true,
                     },
                     {
                         name: "created_at",
