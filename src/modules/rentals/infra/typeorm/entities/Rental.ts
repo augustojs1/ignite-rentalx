e188c98c94448162ebc9d0b3cf25a5a4a3dfaad9
Augusto Souza <augustojsouza1@gmail.com>
2021-04-12 16:19:40 -0300

    Finished rental listing functionality

--- src/modules/rentals/infra/typeorm/entities/Rental.ts
+++ src/modules/rentals/infra/typeorm/entities/Rental.ts
@@ -1,41 +1,54 @@
-import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from "typeorm";
-import { v4 as uuidv4 } from "uuid"
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
 
+import { Car } from '@modules/cars/infra/typeorm/entities/Car';
 
-@Entity("rentals")
+@Entity('rentals')
 class Rental {
-    @PrimaryColumn()
-    id: string;
+  @PrimaryColumn()
+  id: string;
 
-    @Column()
-    car_id: string;
-   
-    @Column()
-    user_id: string;
+  @ManyToOne(() => Car)
+  @JoinColumn({ name: 'car_id' })
+  car: Car;
 
-    @Column()
-    start_date: Date;
+  @Column()
+  car_id: string;
 
-    @Column()
-    end_date: Date;
+  @Column()
+  user_id: string;
 
-    @Column()
-    expected_return_date: Date;
+  @Column()
+  start_date: Date;
 
-    @Column()
-    total: number;
+  @Column()
+  end_date: Date;
 
-    @CreateDateColumn()
-    created_at: Date;
+  @Column()
+  expected_return_date: Date;
 
-    @UpdateDateColumn()
-    updated_at: Date;
+  @Column()
+  total: number;
 
-    constructor() {
-        if(!this.id) {
-            this.id = uuidv4();
-        }
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidv4();
     }
+  }
 }
 
 export { Rental };
