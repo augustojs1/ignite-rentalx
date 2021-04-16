98368e922382a38f6dea6c378bea93222f859e1e
Augusto Souza <augustojsouza1@gmail.com>
2021-04-16 16:44:24 -0300

    testing .env

--- src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
+++ src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
@@ -1,10 +1,11 @@
 import { IStorageProvider } from '../IStorageProvider';
 
 class S3StorageProvider implements IStorageProvider {
-  save(file: string, folder: string): Promise<string> {
+  async save(file: string, folder: string): Promise<string> {
     throw new Error('Method not implemented.');
   }
-  delete(file: string, folder: string): Promise<void> {
+
+  async delete(file: string, folder: string): Promise<void> {
     throw new Error('Method not implemented.');
   }
 }
